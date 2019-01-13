4392c93c33d51717eb7d904491d38615266c7841
AD001\z002txtf <codruta.ene@siemens.com>
2019-01-13 21:47:48 +0200

    add frontend form to submit

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,16 @@ socket.on('message', function(message) {
 	console.log("new message!");
 	console.log(message.text);
 });
+
+// handle submit new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+	event.preventDefault();
+
+	socket.emit('message', {
+		text: $form.find('input[name=message]').val()
+	});
+
+	$form.find('input[name=message]').val('') ;
+});
